c3f6f68e9a6d48fb7d9303d47e28288b3abba9a4
MrBlueSparks <rhysbarker99@gmail.com>
2025-11-03 15:07:53 -0700

    added image folder, set up basic view containers

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.write("<h1 class='text-3xl font-bold text-blue-600'>Hello Tailwind from JS 👋</h1>");
 
-function hideElementById(id) {
-    document.getElementById(id).style.visibility = 'hidden';
+// Function to toggle the display of a view
+function toggleViewDisplay(elementId) {
+    const element = document.getElementById(elementId);
+    element.setAttribute('style', element.style.display === 'none' ? 'display: block;' : 'display: none;');
 }
 
-hideElementById('view1');
+
+
