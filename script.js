aa1720ad1c2f7f23c1e5043a74db92bd57be706d
MrBlueSparks <rhysbarker99@gmail.com>
2025-11-12 11:19:19 -0700

    small tweaks

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-
-
-//const navLink = document.querySelectorAll('p a');
-
-//using event delegation for each nav element
 const navLinksListner = document.querySelector("nav").addEventListener("click", focusOnView);
-
 
 function focusOnView(e){
     if(e.target.tagName === "A"){
         const viewId = e.target.getAttribute("href").substring(1); //get the id without the #
         const views = document.querySelectorAll("main article");
-        
-        views.forEach((view) => {
-            if(view.id === viewId){
-                view.classList.remove("invisible");
-            } else {
-                view.classList.add("invisible");
-            }
-        });
+        for (let view of views) {
+             view.id == viewId ? view.classList.remove("invisible") : view.classList.add("invisible");
+        }
     }
 }
 
